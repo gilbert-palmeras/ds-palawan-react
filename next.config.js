c5893736c780eafaa3246ae09904fc00fa3c3416
Gilbert Palmeras <gpalmeras@foxgroupint.com>
2023-10-23 13:54:00 +0800

    WIP

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  webpack: config => {
+    config.ignoreWarnings = [
+      { module: /node_modules\/next\/dist\/webpack\/loaders\/css-loaders\/src\/index\.js/ },
+      { file: /node_modules\/next\/dist\/webpack\/loaders\/css-loaders\/src\/index\.js/ },
+    ];
+  
+    return config;
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  
 }
 
 module.exports = nextConfig
